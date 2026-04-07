203f4350a2f291abc5273a9224eb76aef0dd1993
BucioDev <buciodev@gmail.com>
2026-04-06 18:41:30 -0700

    fix sys.argv error

--- src/main.py
+++ src/main.py
@@ -7,15 +7,16 @@ dir_path_static = "./static"
 dir_path_public = "./docs" #kinda
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
     node = TextNode("This is some anchor text", TextType.LINK,url="https://www.boot.dev")
     print("This text comes form the main")
     print(node)
-    if sys.argv == None:
-        basepath = "/"
-    else:
-        basepath = sys.argv[0]
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     copy_static_to_public()
     
     generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
